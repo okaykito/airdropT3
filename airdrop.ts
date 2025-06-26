d9604da352c01447acebaf993fd42b06effa644e
okaykito <ksupadhyaya27@gmail.com>
2025-06-26 13:15:14 +0530

    Airdropped SOL to Turbine Address

--- airdrop.ts
+++ airdrop.ts
@@ -1,22 +1,51 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
+import { Connection, Keypair, LAMPORTS_PER_SOL, Transaction, SystemProgram, sendAndConfirmTransaction } from "@solana/web3.js"
 import wallet from "./dev-wallet.json"
-// We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-// Create a Solana devnet connection to devnet SOL tokens
 const connection = new Connection("https://api.devnet.solana.com");
 
-// coming back in 5 mins with the actual wallet keys (have to dualboot to get them)
-// till now we have updated the dev-wallet.json file with the keypair from the Phantom wallet
-// and wrote code before the claim 2 devnet SOL tokens part
+// Replace this with the recipient's public key, for example:
+const recipientAddress = "8JoFsikRWbsZjT9vji3Mn6vCmECvZWzqGJi82YCcyP1b";
+import { PublicKey } from "@solana/web3.js";
+const to = new PublicKey(recipientAddress);
+
 
 (async () => {
 try {
-// We're going to claim 2 devnet SOL tokens
-const txhash = await
-connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
-
+// Get balance of dev wallet
+const balance = await connection.getBalance(keypair.publicKey)
+// Create a test transaction to calculate fees
+const transaction = new Transaction().add( 
+SystemProgram.transfer({
+fromPubkey: keypair.publicKey,
+toPubkey: to, 
+lamports: balance,
+})
+);
+transaction.recentBlockhash = (await
+connection.getLatestBlockhash('confirmed')).blockhash;
+transaction.feePayer = keypair.publicKey;
+// Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+const fee = (await
+connection.getFeeForMessage(transaction.compileMessage(),
+'confirmed')).value || 0;
+// Remove our transfer instruction to replace it
+transaction.instructions.pop();
+// Now add the instruction back with correct amount of lamports
+transaction.add(
+SystemProgram.transfer({
+fromPubkey: keypair.publicKey,
+toPubkey: to,
+lamports: balance - fee,
+})
+);
+// Sign transaction, broadcast, and confirm
+const signature = await sendAndConfirmTransaction( 
+  connection,
+  transaction,
+  [keypair]
+);
 console.log(`Success! Check out your TX here:
-https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+https://explorer.solana.com/tx/${signature}?cluster=devnet`)
 } catch(e) {
 console.error(`Oops, something went wrong: ${e}`)
 }
