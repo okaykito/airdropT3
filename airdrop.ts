b69c5a7b7dffc84e2f23acb8382a5ee17f816d77
okaykito <ksupadhyaya27@gmail.com>
2025-06-26 12:54:43 +0530

    Airdroppped meself some SOL

--- airdrop.ts
+++ airdrop.ts
@@ -8,3 +8,16 @@ const connection = new Connection("https://api.devnet.solana.com");
 // coming back in 5 mins with the actual wallet keys (have to dualboot to get them)
 // till now we have updated the dev-wallet.json file with the keypair from the Phantom wallet
 // and wrote code before the claim 2 devnet SOL tokens part
+
+(async () => {
+try {
+// We're going to claim 2 devnet SOL tokens
+const txhash = await
+connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+
+console.log(`Success! Check out your TX here:
+https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+} catch(e) {
+console.error(`Oops, something went wrong: ${e}`)
+}
+})();
