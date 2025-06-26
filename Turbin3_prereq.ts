4fed006707ff1e2f6b89c1faf7281d660672d3c9
okaykito <ksupadhyaya27@gmail.com>
2025-06-26 18:47:42 +0530

    Reset here

---
+++ Turbin3_prereq.ts
@@ -0,0 +1,19 @@
+export type Turbin3Prereq = { version: "0.1.0"; name: "Turbin3_prereq" };
+ export const IDL: Turbin3Prereq = { version: "0.1.0", name: "Turbin3_prereq" };
+////export const IDL = {
+//  version: "0.1.0",
+//  name: "turbin3_prereq",
+//  instructions: [
+//    {
+//      name: "initialize",
+//      accounts: [
+//        { name: "user", isMut: true, isSigner: true },
+//        { name: "account", isMut: true, isSigner: false },
+//        { name: "systemProgram", isMut: false, isSigner: false },
+//      ],
+//      args: [
+//        { name: "githubUsername", type: "string" }
+//      ]
+//    }
+//  ]
+//};
